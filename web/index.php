b4ad4fd3df419c1bbc8d1640a14d2c9c4b584435
deivisstasuls <deivis.stasuls@gmail.com>
2024-09-26 10:11:48 +0300

    3.task half done

---
+++ web/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Piesakies webināram</h1>
+    <br>
+    <div id="Form_Content">
+    <form action="">
+        <label for="1">Email:</label>
+        <input type="text" name="email_input" id="1">
+        <label for="2">Level:</label>
+        <select name="Levels" id="2"></select>
+        <label for="3">Yes:</label>
+        <input type="radio" name="radio_yes/no" id="3">
+        <label for="4">No:</label>
+        <input type="radio" name="radio_yes/no" id="4">
+        
+
+
+    </form>
+    <a href="blog.html">Biografy</a>
+    </div>
+
+    
+</body>
+</html>
+
