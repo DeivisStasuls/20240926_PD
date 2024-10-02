2d7252430bbd288230252974e322f3477fb90d9a
deivisstasuls <deivis.stasuls@gmail.com>
2024-10-02 16:57:42 +0300

    commited by R.Sirotins

---
+++ web/app.js
@@ -0,0 +1,1 @@
+console.log("Deivis Stašuls c:<");
--- web/index.php
+++ web/index.php
@@ -4,8 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="app.js"></script>
 </head>
 <body>
+    <h4>menue:
+    <a href="blog.html">Biografy</a></h4>
+
     <h1>Piesakies webināram</h1>
     <br>
     <div id="Form_Content">
@@ -13,16 +17,25 @@
         <label for="1">Email:</label>
         <input type="text" name="email_input" id="1">
         <label for="2">Level:</label>
-        <select name="Levels" id="2"></select>
+        <select name="Levels" id="2">
+            <option value="first_lvl">Beginner</option>
+            <option value="second_lvl">Intermidiate</option>
+            <option value="third_lvl">Advanced</option>
+        </select>
+
+
+        <h3>Vai velaties sanemt info par citiem musu organizetajiem pasakumiem?:</h3>
+    
         <label for="3">Yes:</label>
         <input type="radio" name="radio_yes/no" id="3">
         <label for="4">No:</label>
         <input type="radio" name="radio_yes/no" id="4">
-        
+        <br>
+        <button type="submit">DONE</button>
 
 
     </form>
-    <a href="blog.html">Biografy</a>
+    <a href="blog.html">Page 2</a>
     </div>
 
     
